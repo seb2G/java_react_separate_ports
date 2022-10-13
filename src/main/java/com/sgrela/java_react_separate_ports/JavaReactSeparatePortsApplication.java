43d86e822fdd9861bfd651b6e4cfc08b1a790755
sebg <seb2.grela@gmail.com>
2022-10-13 23:49:00 +0100

    initial commit

---
+++ src/main/java/com/sgrela/java_react_separate_ports/JavaReactSeparatePortsApplication.java
@@ -0,0 +1,13 @@
+package com.sgrela.java_react_separate_ports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaReactSeparatePortsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaReactSeparatePortsApplication.class, args);
+	}
+
+}
